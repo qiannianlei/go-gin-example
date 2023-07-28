aadc19384e92e2aa607ca33189628cef54915c83
jinliangping <1441059672@qq.com>
2023-07-28 16:06:24 +0800

    init project

--- models/models.go
+++ models/models.go
@@ -7,7 +7,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 
-	"github.com/qiannianlei/go-gin-example/pkg/setting"
+	"gin-blog/pkg/setting"
 )
 
 var db *gorm.DB
--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/unknwon/com"
 
-	"github.com/qiannianlei/go-gin-example/pkg/setting"
+	"gin-blog/pkg/setting"
 )
 
 func GetPage(c *gin.Context) int {
